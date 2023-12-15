aa790b9a3393b574bbef39ea0e7789b8c00f25d3
Vinayak Gokhale <Vinayak.Gokhale@amd.com>
2023-12-15 17:09:06 +0000

    Add description of supported features

--- python/perf-kernels/flash-attention.py
+++ python/perf-kernels/flash-attention.py
@@ -3,11 +3,18 @@ Fused Attention
 ===============
 
 This is a Triton implementation of the Flash Attention v2 algorithm from Tri Dao (https://tridao.me/publications/flash2/flash2.pdf)
-Credits: OpenAI kernel team
+Credits: OpenAI kernel team, AMD ML Frameworks Triton team
 
-Extra Credits:
-- Original flash attention paper (https://arxiv.org/abs/2205.14135)
-- Rabe and Staats (https://arxiv.org/pdf/2112.05682v2.pdf)
+Features supported:
+
+1) Fwd + bwd kernel with causal masking
+2) Vector and matrix bias (currently fwd kernel only, no causal masking)
+3) Any sequence lengths without padding (currently fwd kernel only, no causal masking)
+4) fp8 (e5m2fnuz, QK GEMM in fwd kernel only)
+
+Not currently supported:
+
+1) Nested / ragged tensors ("varlen")
 
 """
 
