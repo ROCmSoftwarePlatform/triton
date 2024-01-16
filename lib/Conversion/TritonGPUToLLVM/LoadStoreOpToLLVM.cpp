886bbdc048fb49aafa7e949ab550f555a432946c
Hongtao Yu <hoy@fb.com>
2024-01-16 11:32:04 -0800

    Emit vectorized loads

--- lib/Conversion/TritonGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -156,57 +156,46 @@ struct LoadOpConversion
 
 #ifdef USE_ROCM
       Value pred = mask ? maskElems[vecStart] : int_val(1, 1);
+      auto vecTy = LLVM::getFixedVectorType(valueElemTy, vec);
       auto loaded = rewriter.create<scf::IfOp>(
           loc,
           pred,
           [&](OpBuilder& builder, Location loc) {
-            SmallVector<Value> thenLoadValues;
-            for (size_t wordIdx = 0; wordIdx < nWords; ++wordIdx) {
-              size_t elemOffset = vecStart + wordIdx * wordNElems;
-              Value ptr = addrspacecast(
-                  ptrElems[elemOffset],
-                  ptr_ty(IntegerType::get(getContext(), width)));
-              auto loadVal = rewriter.create<LLVM::LoadOp>(loc, ptr);
-              thenLoadValues.push_back(loadVal);
-            }
-            builder.create<mlir::scf::YieldOp>(loc, thenLoadValues);
+
+            Value ptr = addrspacecast(ptrElems[vecStart], ptr_ty(vecTy));
+            auto loadVal = rewriter.create<LLVM::LoadOp>(loc, ptr);
+            builder.create<mlir::scf::YieldOp>(loc, ValueRange({loadVal}));
           },
           [&](OpBuilder& builder, Location loc) {
-            SmallVector<Value> elseLoadValues;
-            for (size_t wordIdx = 0; wordIdx < nWords; ++wordIdx) {
-              size_t elemOffset = vecStart + wordIdx * wordNElems;
-              Value zeroVal = int_val(width, 0);
-              Value otherVal;
-              if (other) {
-                auto vecTy = LLVM::getFixedVectorType(valueElemTy, wordNElems);
-                Value v = undef(vecTy);
-                for (size_t s = 0; s < wordNElems; ++s) {
-                  Value falseVal = otherElems[elemOffset + s];
-                  Value sVal = createIndexAttrConstant(
-                      rewriter,
-                      loc,
-                      this->getTypeConverter()->getIndexType(),
-                      s);
-                  v = insert_element(vecTy, v, falseVal, sVal);
-                }
-                otherVal = bitcast(v, IntegerType::get(getContext(), width));
+            mlir::Attribute zero = builder.getZeroAttr(valueElemTy);
+            auto denseValue =
+                DenseElementsAttr::get(vecTy.cast<mlir::ShapedType>(), zero);
+            Value zeroVal =
+                rewriter.create<LLVM::ConstantOp>(loc, vecTy, denseValue);
+            Value otherVal;
+            if (other) {
+              Value v = undef(vecTy);
+              for (size_t s = 0; s < vec; ++s) {
+                Value falseVal = otherElems[vecStart + s];
+                Value sVal = createIndexAttrConstant(
+                    rewriter, loc, this->getTypeConverter()->getIndexType(), s);
+                v = insert_element(vecTy, v, falseVal, sVal);
               }
-              Value falseVal = other ? otherVal : zeroVal;
-              elseLoadValues.push_back(falseVal);
+              otherVal = v;
             }
-            builder.create<mlir::scf::YieldOp>(loc, elseLoadValues);
+            Value falseVal = other ? otherVal : zeroVal;
+            builder.create<mlir::scf::YieldOp>(loc, ValueRange({falseVal}));
           });
 
-      for (size_t wordIdx = 0; wordIdx < nWords; ++wordIdx) {
-        Value loadVal = bitcast(
-            loaded->getResult(wordIdx),
-            LLVM::getFixedVectorType(valueElemTy, wordNElems));
-        for (size_t ii = 0; ii < wordNElems; ++ii) {
-          Value vecIdx = createIndexAttrConstant(
-              rewriter, loc, this->getTypeConverter()->getIndexType(), ii % wordNElems);
-          Value loaded = extract_element(valueElemTy, loadVal, vecIdx);
-          loadedVals.push_back(loaded);
-        }
+      Value loadVal = bitcast(loaded->getResult(0), vecTy);
+      for (size_t ii = 0; ii < vec; ++ii) {
+        Value vecIdx = createIndexAttrConstant(
+            rewriter,
+            loc,
+            this->getTypeConverter()->getIndexType(),
+            ii % vec);
+        Value loaded = extract_element(valueElemTy, loadVal, vecIdx);
+        loadedVals.push_back(loaded);
       }
 #else
       // TODO(Superjomn) Add cache policy fields to StoreOp.
