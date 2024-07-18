bfbc3ef8baa7db9c3568407370f7a91d0a6005b1
Vinayak Gokhale <Vinayak.Gokhale@amd.com>
2024-07-18 22:51:25 +0000

    Fix bug

--- python/perf-kernels/flash-attention.py
+++ python/perf-kernels/flash-attention.py
@@ -1171,6 +1171,7 @@ def test_op_fwd_bias(Z, H, N_CTX_Q, N_CTX_K, D_HEAD, causal, use_bias, dtype):
     if causal:
         input_metadata.need_causal()
     if use_bias:
+        bias = torch.randn((1, H, N_CTX_Q, N_CTX_K), dtype=dtype, device="cuda")
         input_metadata.need_bias(bias, Z, H, N_CTX_Q, N_CTX_K)
     else:
         bias = None
