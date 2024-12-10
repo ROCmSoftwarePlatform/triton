72192d58b8757af4af16db36528627cb415dcc8b
Vinayak Gokhale <Vinayak.Gokhale@amd.com>
2024-12-10 09:15:20 +0000

    Cleanup

--- python/perf-kernels/gemm.py
+++ python/perf-kernels/gemm.py
@@ -1,5 +1,4 @@
 import torch
-
 import triton
 import triton.language as tl
 import sys
@@ -40,17 +39,12 @@ import re
 })
 @triton.jit
 def matmul_kernel(
-    # Pointers to matrices
     a_ptr,
     b_ptr,
     c_ptr,
-    # Matrix dimensions
     M,
     N,
     K,
-    # The stride variables represent how much to increase the ptr by when moving by 1
-    # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
-    # by to get the element one row down (A has M rows).
     stride_am,
     stride_ak,
     stride_bk,
@@ -73,7 +67,7 @@ def matmul_kernel(
     # -----------------------------------------------------------
     # Map program ids `pid` to the block of C it should compute.
     # This is done in a grouped ordering to promote L2 data reuse.
-    # See above `L2 Cache Optimizations` section for details.
+    # TODO(vgokhale): Add XCD remapping.
     pid = tl.program_id(axis=0)
     num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
     num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
@@ -88,24 +82,13 @@ def matmul_kernel(
         pid_m = first_pid_m + (pid % group_size_m)
         pid_n = (pid % num_pid_in_group) // group_size_m
 
-    # ----------------------------------------------------------
-    # Create pointers for the first blocks of A and B.
-    # We will advance this pointer as we move in the K direction
-    # and accumulate
-    # `a_ptrs` is a block of [BLOCK_SIZE_M, BLOCK_SIZE_K] pointers
-    # `b_ptrs` is a block of [BLOCK_SIZE_K, BLOCK_SIZE_N] pointers
-    # See above `Pointer Arithmetics` section for details
+    # Create pointers for first block of A and B input matrices
     offs_k = tl.arange(0, BLOCK_SIZE_K)
     offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
     offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
     a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
     b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
 
-    # -----------------------------------------------------------
-    # Iterate to compute a block of the C matrix.
-    # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
-    # of fp32 values for higher accuracy.
-    # `accumulator` will be converted back to fp16 after the loop.
     acc_dtype = tl.float32 if c_ptr.type.element_ty != tl.int8 else tl.int32
     accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=acc_dtype)
 
@@ -118,21 +101,19 @@ def matmul_kernel(
         else:
             a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
             b = tl.load(b_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
-        # We accumulate along the K dimension.
         accumulator += tl.dot(a, b)
 
         # Advance the ptrs to the next K block.
         a_ptrs += BLOCK_SIZE_K * stride_ak
         b_ptrs += BLOCK_SIZE_K * stride_bk
-    # You can fuse arbitrary activation functions here
-    # while the accumulator is still in FP32!
+    # Apply activation function, if specified.
     if ACTIVATION == "leaky_relu":
         accumulator = leaky_relu(accumulator)
+    # Apply scale to recover dynamic range reduced due to lower precision inputs.
     if APPLY_SCALE:
         accumulator = accumulator * scale
     c = accumulator.to(c_ptr.type.element_ty)
 
-    # -----------------------------------------------------------
     # Write back the block of the output matrix C with masks.
     offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
     offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
@@ -141,29 +122,25 @@ def matmul_kernel(
     tl.store(c_ptrs, c, mask=c_mask)
 
 
-# We can fuse `leaky_relu` by providing it as an `ACTIVATION` meta-parameter in `_matmul`.
+# Activation function.
 @triton.jit
 def leaky_relu(x):
     x = x + 1
     return tl.where(x >= 0, x, 0.01 * x)
 
 
-# %%
-# We can now create a convenience wrapper function that only takes two input tensors,
-# and (1) checks any shape constraint; (2) allocates the output; (3) launches the above kernel.
-
-
+# Wrapper for gemm kernel.
 def matmul(a, b, c, a_scale, b_scale, activation=""):
     # Check constraints.
-    assert a.shape[1] == b.shape[0], "Incompatible dimensions"
-    # assert a.is_contiguous(), "Matrix A must be contiguous"
-    # assert b.is_contiguous(), "Matrix B must be contiguous"
+    assert a.shape[1] == b.shape[0], "Incompatible dimensions!!!"
+    assert a.dtype == b.dtype, "Mixed dtype GEMMs are not supported!!!"
     M, K = a.shape
     K, N = b.shape
-    # 1D launch kernel where each block gets its own program.
     grid = lambda META: (triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']), )
     apply_scale = a_scale is not None and b_scale is not None
     scale = a_scale * b_scale if apply_scale else None
+    if a.dtype == torch.int8 or a.dtype == torch.int32:
+        scale = int(scale)
     matmul_kernel[grid](
         a,
         b,
@@ -181,7 +158,6 @@ def matmul(a, b, c, a_scale, b_scale, activation=""):
         APPLY_SCALE=apply_scale,
         ACTIVATION=activation,
     )
-
 
 name_to_torch_types = {
     'int8': torch.int8,
@@ -223,11 +199,6 @@ def gen_input(M, N, dtype, needTrans, seed, device='cuda'):
     return input, input_f32, scale
 
 
-# %%
-# Unit Test
-# ---------
-#
-# We can test our custom matrix multiplication operation against a native torch implementation (i.e., rocBLAS).
 def get_x_vals():
     x_vals = [(1024 * v, 1024 * v, 1024 * v) for v in range(1, 9)]
 
@@ -236,19 +207,19 @@ def get_x_vals():
     return x_vals
 
 
+# Unit tests
 @pytest.mark.parametrize("M, N, K, in_dtype, out_dtype, col_a, col_b", [
     (*shape, in_dtype, out_dtype, col_a, col_b)
     for shape in get_x_vals()
-    for in_dtype, out_dtype in [('fp16', 'fp16'), 
-                                ('bf16', 'bf16'), 
-                                ('fp32','fp32'), 
-                                ('fp8e4','fp16'), ('fp8e5', 'fp16'),
+    for in_dtype, out_dtype in [('fp16', 'fp16'),
+                                ('bf16', 'bf16'),
+                                ('fp32','fp32'),
+                                ('fp8e4','fp16'),
+                                ('fp8e5', 'fp16'),
                                 ('int8', 'int8'),
                                 ('int8', 'int32')
                                 ]
-    # Only test k-major tensors because
-    # 1. This is the most preformant config and the current focus
-    # 2. Other case does not work with num_stages=0 (TODO (zhanglx))
+    # Defines if a matrix is row or column major.
     for col_a in [True, False]
     for col_b in [True, False]
 ])
@@ -256,13 +227,16 @@ def test_correctness(M, N, K, col_a, col_b, in_dtype, out_dtype):
     torch_in_dtype = name_to_torch_types[in_dtype]
     a, a_fp32, a_scale = gen_input(M, K, torch_in_dtype, col_a, 1, device='cuda')
     b, b_fp32, b_scale = gen_input(K, N, torch_in_dtype, col_b, 2, device='cuda')
-    # Allocates output.
     torch_out_dtype = name_to_torch_types[out_dtype]
     c = torch.empty((M, N), device=a.device, dtype=torch_out_dtype)
+    # For 8-bit, we have scaled to the dynamic range of the data type.
+    # This requires us to compute in fp32 because for e5m2, the range is same as fp16 (e5m10).
+    # If we use fp16 it is possible to return infs from the torch.matmul call.
     if dtype_is_8_bit(torch_in_dtype):
         matmul(a, b, c, a_scale.item(), b_scale.item(), activation="")
         torch_output = torch.matmul(a_fp32, b_fp32)
         torch_output = torch_output * a_scale * b_scale
+    # For other dtypes, use the same torch matmul as the dtype.
     else:
         matmul(a, b, c, a_scale=None, b_scale=None, activation="")
         torch_output = torch.matmul(a.to(torch_in_dtype), b.to(torch_in_dtype))
@@ -272,48 +246,25 @@ def test_correctness(M, N, K, col_a, col_b, in_dtype, out_dtype):
         torch.testing.assert_close(c, torch_output.to(torch_out_dtype), atol=5e-3, rtol=1e-2)
 
 
-# %%
-# Benchmark
-# ---------
-#
-# Square Matrix Performance
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# We can now compare the performance of our kernel against that of rocBLAS. Here we focus on square matrices,
-# but feel free to arrange this script as you wish to benchmark any other matrix shape.
-
-
 def get_type(provider):
     res = re.findall(r'\(.*?\)', provider)
     return res[0][1:-1]
 
 
-inout_dtype = {
-    'int8': torch.int8,
-    'fp16': torch.float16,
-    'fp32': torch.float32,
-    'bf16': torch.bfloat16,
-    'fp8e4': torch.float16,
-    'fp8e5': torch.float16,
-}
-
-
 @triton.testing.perf_report(
     triton.testing.Benchmark(
-        x_names=['M', 'N', 'K'],  # Argument names to use as an x-axis for the plot
+        x_names=['M', 'N', 'K'],
         x_vals=get_x_vals(),
-        line_arg='provider',  # Argument name whose value corresponds to a different line in the plot
-        # Possible values for `line_arg`
+        line_arg='provider',
         line_vals=[
             'rocblas(fp16)', 'rocblas(bf16)', 'triton(fp16)', 'triton(bf16)', 'triton(int8)', 'triton(fp8e4)',
             'triton(fp8e5)'
         ],
-        # Label name for the lines
         line_names=[
             "rocBLAS.Fp16", "rocBLAS.Bf16", "Triton.Fp16", "Triton.Bf16", "Triton.Int8", "Triton.Fp8E4", "Triton.Fp8E5"
         ],
-        ylabel="TFLOPS",  # Label name for the y-axis
-        plot_name="matmul-performance",  # Name for the plot, used also as a file name for saving the plot.
+        ylabel="TFLOPS",
+        plot_name="matmul-performance",
         args={},
     ))
 def benchmark(M, N, K, provider):
@@ -344,6 +295,7 @@ def benchmark(M, N, K, provider):
     return perf(ms), perf(max_ms), perf(min_ms)
 
 
+# TODO(vgokhale): Add more options to benchmarking
 def parse_args():
     parser = argparse.ArgumentParser(
         prog="GEMM tutorial example",
