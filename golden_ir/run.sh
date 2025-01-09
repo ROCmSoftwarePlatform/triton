f704362f2950c56a144d0f5a2a5a5bc6274fdaeb
David Tanner <dtanner@amd.com>
2025-01-09 20:16:57 +0000

    Workspace to creating and launching custom ttgir and llir.

---
+++ golden_ir/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -rf triton_cache
+
+TRITON_MFMA_TILE_ENABLE_SCHED_BARRIERS=1 TRITON_CACHE_DIR=triton_cache python gemm_sub.py \
+  --trans-b \
+  --file config.yaml \
+  --verbose
+
+# remember groupm of llir must match grid launch
+
+#  --sub ir/mfma_order_sb_nomask_orig.ttgir \    # =1 575
+#  --sub ir/mfma_order_sb_nomask_orig.llir \     # =1 575 passes
+
+#  --use-mask \
+#  --verbose
+#  --dump-ir amdgcn \
+# TRITON_HIP_USE_BLOCK_PINGPONG=1
+# MLIR_ENABLE_DUMP=1
+
