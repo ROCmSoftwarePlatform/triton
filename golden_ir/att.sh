ed916efdb84441a0f1d073e745aac0abe987b781
David Tanner <dtanner@amd.com>
2025-01-10 21:03:04 +0000

    att viewer gets correct asm but halts parsing

--- golden_ir/att.sh
+++ golden_ir/att.sh
@@ -6,9 +6,9 @@ rm -rf triton_cache
 #TRITON_MFMA_TILE_ENABLE_SCHED_BARRIERS=1 TRITON_CACHE_DIR=triton_cache rocprofv2 \
 AMDGCN=/home/dtanner/repos/rocm_triton/golden_ir/matmul_kernel.amdgcn
 rocprofv2 \
-  -d att_2d \
+  -d att_3a \
   -i att.txt \
-  --plugin att auto \
+  --plugin att ${AMDGCN} \
   --mode file \
   ./att_exec.sh
 
