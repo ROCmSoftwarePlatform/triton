3cfe33178897488b474aef19ab2a683d229b1ec1
ravil-mobile <ravil.aviva.com@gmail.com>
2025-01-24 11:17:35 +0000

    [AMD] Added a proto of the ConcatOp to the AMDGPU dialect

--- third_party/amd/lib/Dialect/TritonAMDGPU/IR/Dialect.cpp
+++ third_party/amd/lib/Dialect/TritonAMDGPU/IR/Dialect.cpp
@@ -133,4 +133,23 @@ LogicalResult ExtractSliceOp::verify() {
 
   return success();
 }
+
+LogicalResult ConcatOp::verify() {
+  auto sources = getSources();
+  auto dims = getDims();
+
+  auto expectedNumSources =
+      std::accumulate(dims.begin(), dims.end(), 1, std::multiplies<int32_t>());
+  if (sources.size() != expectedNumSources) {
+    return emitError() << "dims spec [" << dims
+                       << "] does not match the number of provided sources ["
+                       << sources.size() << "]";
+  }
+
+  // TODO: check whether all sources and the result have the same encoding
+
+  // TODO: check whether assembled sources match the result dimensions
+
+  return success();
+}
 } // namespace mlir::triton::amdgpu
